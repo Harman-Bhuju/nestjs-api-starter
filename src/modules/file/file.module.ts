223f79af3f020916fd3771a97883775d9f95516d
Harman Bhuju <harmanbhuju@gmail.com>
2026-08-03 17:05:00 +0545

    fix(file): correct import statement for file entity

--- src/modules/file/file.module.ts
+++ src/modules/file/file.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { File } from 'src/modules/file/entities/file.entity';
 import { FileService } from './service/file.service';
 import { CloudinaryService } from './service/cloudinary.service';
 import { FileController } from './controller/file.controller';
